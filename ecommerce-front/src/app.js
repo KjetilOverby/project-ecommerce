ac043a81ed20c62575b42432554a5151c453b4c2
Kjetil Øverby <kjetiloverby@Kjetils-iMac.local>
2019-09-19 21:21:45 +0200

    react

---
+++ ecommerce-front/src/app.js
@@ -0,0 +1,20 @@
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+import AppIndex from './AppIndex'
+
+ReactDOM.render(<AppIndex />, document.getElementById('app'))
+
+
+
+
+
+
+
+
+
